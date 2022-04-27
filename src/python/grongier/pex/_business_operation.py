e656d144bb0aae4d210d2c5adcc51105c5798624
grongierisc <guillaume.rongier@intersystems.com>
2022-04-27 15:02:34 +0000

    init pythonic bo

--- src/python/grongier/pex/_business_operation.py
+++ src/python/grongier/pex/_business_operation.py
@@ -11,27 +11,24 @@ class _BusinessOperation(_BusinessHost):
     """
 
     DISPATCH = []
+    adapter = None
 
     def __init__(self):
         """ The adapter variable provides access to the outbound adapter associated with the business operation."""
         super().__init__()
-        self.Adapter = None
 
-    def OnInit(self):
-        """ The OnInit() method is called when the component is started.
-        Use the OnInit() method to initialize any structures needed by the component."""
-        pass
+    def on_init(self):
+        """ The on_init() method is called when the component is started.
+        Use the on_init() method to initialize any structures needed by the component."""
 
-    def OnTearDown(self):
+    def on_tear_down(self):
         """ Called before the component is terminated. Use it to freee any structures."""
-        pass
 
-    def OnConnected(self):
-        """ The OnConnected() method is called when the component is connected or reconnected after being disconnected.
-        Use the OnConnected() method to initialize any structures needed by the component."""
-        pass
+    def on_connected(self):
+        """ The on_connected() method is called when the component is connected or reconnected after being disconnected.
+        Use the on_connected() method to initialize any structures needed by the component."""
 
-    def OnMessage(self, request):
+    def on_message(self, request):
         """ Called when the business operation receives a message from another production component.
         Typically, the operation will either send the message to the external system or forward it to a business process or another business operation.
         If the operation has an adapter, it uses the Adapter.invoke() method to call the method on the adapter that sends the message to the external system.
@@ -53,43 +50,43 @@ class _BusinessOperation(_BusinessHost):
         """
         return ""
 
-    def _setIrisHandles(self, handleCurrent, handlePartner):
+    def _setIrisHandles(self, handle_current, handle_partner):
         """ For internal use only. """
-        self.irisHandle = handleCurrent
-        if type(handlePartner).__module__.find('iris') == 0:
-            if handlePartner._IsA("Grongier.PEX.OutboundAdapter"):
-                module = importlib.import_module(handlePartner.GetModule())
-                handlePartner = getattr(module, handlePartner.GetClassname())()
-            self.Adapter = handlePartner
+        self.irisHandle = handle_current
+        if type(handle_partner).__module__.find('iris') == 0:
+            if handle_partner._IsA("Grongier.PEX.OutboundAdapter"):
+                module = importlib.import_module(handle_partner.GetModule())
+                handle_partner = getattr(module, handle_partner.GetClassname())()
+            self.Adapter = handle_partner
         return
 
-    def _dispatchOnConnected(self, hostObject):
+    def _dispatch_on_connected(self, hostObject):
         """ For internal use only. """
-        self.OnConnected()
+        self.on_connected()
         return
 
-    def _dispatchOnInit(self, hostObject):
+    def _dispatch_on_init(self, hostObject):
         """ For internal use only. """
-        self._createDispatch()
-        self.OnInit()
+        self._create_dispatch()
+        self.on_init()
         return
 
-    def _dispatchOnTearDown(self, hostObject):
+    def _dispatch_on_tear_down(self, hostObject):
         """ For internal use only. """
-        self.OnTearDown()
+        self.on_tear_down()
         return
 
-    def _dispatchOnMessage(self, request):
+    def _dispatch_on_message(self, request):
         """ For internal use only. """
         if isinstance(request, str):
             request = self._deserialize(request)
         # method dispachMessage
-        returnObject = self._dispachMessage(request)
-        if self._is_message_instance(returnObject):
-            returnObject = self._serialize(returnObject)
-        return returnObject
+        return_object = self._dispach_message(request)
+        if self._is_message_instance(return_object):
+            return_object = self._serialize(return_object)
+        return return_object
 
-    def _dispachMessage(self, request):
+    def _dispach_message(self, request):
         """
         It takes a request object, and returns a response object
         
@@ -97,7 +94,7 @@ class _BusinessOperation(_BusinessHost):
         :return: The return value is the result of the method call.
         """
 
-        call = 'OnMessage'
+        call = 'on_message'
 
         module = request.__class__.__module__
         classname = request.__class__.__name__
@@ -109,7 +106,7 @@ class _BusinessOperation(_BusinessHost):
         return getattr(self,call)(request)
 
     
-    def _createDispatch(self):
+    def _create_dispatch(self):
         """
         It creates a list of tuples, where each tuple contains the name of a class and the name of a method
         that takes an instance of that class as its only argument
@@ -138,3 +135,38 @@ class _BusinessOperation(_BusinessHost):
                     classname = annotation[i+1:j]
                     self.DISPATCH.append((classname,method))
         return
+
+    def OnMessage(self, request):
+        """ DEPRECATED : use on_message
+        Called when the business operation receives a message from another production component.
+        Typically, the operation will either send the message to the external system or forward it to a business process or another business operation.
+        If the operation has an adapter, it uses the Adapter.invoke() method to call the method on the adapter that sends the message to the external system.
+        If the operation is forwarding the message to another production component, it uses the SendRequestAsync() or the SendRequestSync() method
+
+        Parameters:
+        request: An instance of either a subclass of Message or of IRISObject containing the incoming message for the business operation.
+
+        Returns:
+        The response object
+        """
+        return self.on_message(request=request)
+
+    def OnInit(self):
+        """ 
+        DEPRECATED : use on_init
+        The on_init() method is called when the component is started.
+        Use the on_init() method to initialize any structures needed by the component."""
+        return self.on_init()
+
+    def OnTearDown(self):
+        """ 
+        DEPRECATED : use on_tear_down
+        Called before the component is terminated. Use it to freee any structures.
+        """
+        return self.on_tear_down()
+
+    def OnConnected(self):
+        """ DEPRECATED : use on_connected
+        The on_connected() method is called when the component is connected or reconnected after being disconnected.
+        Use the on_connected() method to initialize any structures needed by the component."""
+        return self.on_connected()
