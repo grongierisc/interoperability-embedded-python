2100ac302849aac46fce1ff71e8ed52a71254633
grongierisc <guillaume.rongier@intersystems.com>
2022-04-27 17:11:36 +0000

    refactor outbound adptor

--- src/python/grongier/pex/_outbound_adapter.py
+++ src/python/grongier/pex/_outbound_adapter.py
@@ -2,25 +2,7 @@ from grongier.pex._common import _Common
 
 class _OutboundAdapter(_Common):
     """ Responsible for sending the data to the external system."""
-
-    def __init__(self):
-        """ The BusinessHost variable provides access to the BusinessOperation associated with the OutboundAdapter."""
-        super().__init__()
-        self.BusinessHost = None
-    
-    def OnConnected(self):
-        """ The OnConnected() method is called when the component is connected or reconnected after being disconnected.
-        Use the OnConnected() method to initialize any structures needed by the component."""
-        pass
-
-    def OnInit(self):
-        """ The OnInit() method is called when the component is started.
-        Use the OnInit() method to initialize any structures needed by the component."""
-        pass
-
-    def OnTearDown(self):
-        """ Called before the component is terminated. Use it to freee any structures."""
-        pass
+    BusinessHost = None
 
     def _set_iris_handles(self, handle_current, handle_partner):
         """ For internal use only. """
