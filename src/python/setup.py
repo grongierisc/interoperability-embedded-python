7252666900b68024e1baf59ee59f60cd01e18e69
grongierisc <guillaume.rongier@intersystems.com>
2022-06-02 12:27:20 +0200

    update build wheel

--- src/python/setup.py
+++ src/python/setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="grongier_pex",
-    version="1.2.4",
+    version="1.5.3",
     author="Guillaume Rongier",
     author_email="guillaume.rongier@intersystems.com",
     url="https://www.intersystems.com/",
     description="Python Interoperability kit",
-    packages=['grongier.pex'],
+    packages=['grongier.pex','grongier.dacite'],
     python_requires='>=3.6.6',
 )
