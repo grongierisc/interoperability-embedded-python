2deaf218d8962a5438cd92ad43fd6d0ab075f741
grongierisc <guillaume.rongier@intersystems.com>
2022-07-20 17:50:30 +0200

    fix dispatch on_document for process

--- src/python/grongier/pex/_private_session_process.py
+++ src/python/grongier/pex/_private_session_process.py
@@ -5,14 +5,14 @@ class _PrivateSessionProcess(_BusinessProcess):
     
     @_BusinessHost.input_deserialzer
     @_BusinessHost.output_serialzer
-    def _dispatch_on_document(self, host_object, request):
+    def _dispatch_on_document(self, host_object,source_config_name, request):
         """ For internal use only. """
         self._restore_persistent_properties(host_object)
-        return_object = self.on_document(request)
+        return_object = self.on_document(source_config_name,request)
         self._save_persistent_properties(host_object)
         return return_object
 
-    def on_document(request):
+    def on_document(source_config_name,request):
         pass
 
 
