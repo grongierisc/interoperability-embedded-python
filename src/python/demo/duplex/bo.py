a52c15e67d8b3ac1c26bc2276576b61f5f0e0d21
grongierisc <guillaume.rongier@intersystems.com>
2022-07-25 16:14:01 +0200

    remove onprocessinput

--- src/python/demo/duplex/bo.py
+++ src/python/demo/duplex/bo.py
@@ -9,11 +9,8 @@ class Operation(DuplexOperation):
         """
         return "Duplex.adapter.OperationAdapter"
 
-    def on_process_input(self,input):
-        self.send_document_to_process(iris.cls('Ens.Request')._New())
-
     def on_message(self,input):
-        self.log_info('On message')
+        self.log_info('on_message')
 
     def on_private_session_started(self,source_config_name,self_generated):
         self.log_info('In on_private_session_started')
