c29a115315e1f5ae6ff6d7dd0811a45fe667b72c
grongierisc <guillaume.rongier@intersystems.com>
2022-12-14 17:56:59 +0100

    refactor src

--- src/python/demo/duplex/adapter.py
+++
@@ -1,7 +0,0 @@
-from grongier.pex import InboundAdapter
-
-class OperationAdapter(InboundAdapter):
-
-    def on_task(self):
-        self.log_info('on_task')
-        self.business_host.OnProcessInput()
