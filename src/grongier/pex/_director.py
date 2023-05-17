bf34ee352c986c0d251d58d6c08abf8ead9b7a29
grongierisc <guillaume.rongier@intersystems.com>
2023-05-17 13:46:35 +0200

    initial support of manager

--- src/grongier/pex/_director.py
+++ src/grongier/pex/_director.py
@@ -34,42 +34,6 @@ class _Director():
         iris_object = iris.cls("Grongier.PEX.Director").dispatchCreateBusinessService(target)
         return iris_object
 
-import iris
-
-class _Director():
-    """ The Directorclass is used for nonpolling business services, that is, business services which are not automatically
-    called by the production framework (through the inbound adapter) at the call interval.
-    Instead these business services are created by a custom application by calling the Director.CreateBusinessService() method.
-    """
-
-    @staticmethod
-    def CreateBusinessService(target):
-        """ DEPRECATED : use create_business_service
-        The CreateBusinessService() method initiates the specifiied business service.
-
-        Parameters:
-        connection: an IRISConnection object that specifies the connection to an IRIS instance for Java.
-        target: a string that specifies the name of the business service in the production definition.
-
-        Returns:
-            an object that contains an instance of IRISBusinessService
-        """
-        return _Director.create_business_service(target)
-
-    @staticmethod
-    def create_business_service(target):
-        """ The create_business_service() method initiates the specified business service.
-
-        Parameters:
-        connection: an IRISConnection object that specifies the connection to an IRIS instance for Java.
-        target: a string that specifies the name of the business service in the production definition.
-
-        Returns:
-            an object that contains an instance of IRISBusinessService
-        """
-        iris_object = iris.cls("Grongier.PEX.Director").dispatchCreateBusinessService(target)
-        return iris_object
-
     @staticmethod
     def create_python_business_service(target):
         """ The create_business_service() method initiates the specified business service.
@@ -83,3 +47,34 @@ class _Director():
         """
         iris_object = iris.cls("Grongier.PEX.Director").dispatchCreateBusinessService(target)
         return iris_object.GetClass()
+    
+    ### List of function to manage the production
+    ### start production
+    @staticmethod
+    def start_production(production_name):
+        pass
+
+    ### stop production
+    @staticmethod
+    def stop_production(production_name):
+        pass
+
+    ### restart production
+    @staticmethod
+    def restart_production(production_name):
+        pass
+
+    ### shutdown production
+    @staticmethod
+    def shutdown_production(production_name):
+        pass
+
+    ### update production
+    @staticmethod
+    def update_production(production_name):
+        pass
+
+    ### list production
+    @staticmethod
+    def list_production():
+        pass
--- src/grongier/pex/_utils.py
+++ src/grongier/pex/_utils.py
@@ -2,6 +2,7 @@ import os
 import ast
 import iris
 import inspect
+import xmltodict
 
 class _Utils():
     @staticmethod
@@ -169,11 +170,72 @@ class _Utils():
                 * key: the name of the class
                 * value: an instance of the class
             * PRODUCTIONS
+                list of dictionaries:
                 * key: the name of the production
                 * value: a dictionary containing the settings for the production
         """
-        from settings import CLASSES, PRODUCTIONS
-        for key, value in CLASSES.items():
-            # get the absolute path to current value (the class)
+        # try to load the settings file
+        try:
+            from settings import CLASSES, PRODUCTIONS
+        except ImportError:
+            # return an error if the settings file is not found
+            # and explain how to create it
+            raise ImportError("settings.py file not found. Please create it in the same directory as the main.py file. See the documentation for more information.")
+        _Utils.set_classes_settings(CLASSES)
+        _Utils.set_productions_settings(PRODUCTIONS)
+
+
+    @staticmethod
+    def set_classes_settings(class_items):
+        """
+        It takes a dictionary of classes and returns a dictionary of settings for each class
+        
+        :param class_items: a dictionary of classes
+        :return: a dictionary of settings for each class
+        """
+        for key, value in class_items.items():
             path = os.path.dirname(inspect.getfile(value))
             _Utils.register_component(value.__module__,value.__name__,path,1,key)
+
+    @staticmethod
+    def set_productions_settings(production_list):
+        """
+        It takes a list of dictionaries and registers the productions
+        """
+        # for each production in the list
+        for production in production_list:
+            # get the production name (first key in the dictionary)
+            production_name = list(production.keys())[0]
+            # set the first key to 'production'
+            production['Production'] = production.pop(production_name)
+            # transform the json as an xml
+            xml = _Utils.dict_to_xml(production)
+            # register the production
+            _Utils.register_production(production_name,xml)
+    
+    @staticmethod
+    def dict_to_xml(json):
+        """
+        It takes a json and returns an xml
+        
+        :param json: a json
+        :return: an xml
+        """
+        return xmltodict.unparse(json)  
+    
+    @staticmethod
+    def register_production(production_name,xml):
+        """
+        It takes a production name and an xml and registers the production
+        
+        :param production_name: the name of the production
+        :type production_name: str
+        :param xml: the xml of the production
+        :type xml: str
+        """
+        # split the production name in the package name and the production name
+        # the production name is the last part of the string
+        package = '.'.join(production_name.split('.')[:-1])
+        production_name = production_name.split('.')[-1]
+        # register the production
+        _Utils.raise_on_error(iris.cls('Grongier.PEX.Utils').CreateProduction(package,production_name,xml))
