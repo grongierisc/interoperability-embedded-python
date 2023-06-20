ee48bddfbe394444b4889e7a2b4f270752b99547
grongierisc <guillaume.rongier@intersystems.com>
2023-06-20 16:17:30 +0200

    init dev WSGI

---
+++ demo/python/reddit/app.py
@@ -0,0 +1,10 @@
+def demo_app(environ,start_response):
+    from io import StringIO
+    stdout = StringIO()
+    print("Hello world!", file=stdout)
+    print(file=stdout)
+    h = sorted(environ.items())
+    for k,v in h:
+        print(k,'=',repr(v), file=stdout)
+    start_response("200 OK", [('Content-Type','text/plain; charset=utf-8')])
+    return [stdout.getvalue().encode("utf-8")]
---
+++ src/grongier/pex/wsgi/handlers.py
@@ -0,0 +1,44 @@
+from wsgiref.handlers import BaseHandler
+import iris
+
+class IrisHandler(BaseHandler):
+    """Handler that's just initialized with streams, environment, etc.
+
+    This handler subclass is intended for synchronous HTTP/1.0 origin servers,
+    and handles sending the entire response output, given the correct inputs.
+
+    Usage::
+
+        handler = SimpleHandler(
+            inp,out,err,env, multithread=False, multiprocess=True
+        )
+        handler.run(app)"""
+
+    server_software = "IrisWSGI/0.1"
+    wsgi_file_wrapper = None
+
+    def __init__(self,stdin,stdout,stderr,environ,
+        multithread=True, multiprocess=False
+    ):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.base_env = environ
+        self.wsgi_multithread = multithread
+        self.wsgi_multiprocess = multiprocess
+
+    def get_stdin(self):
+        return self.stdin
+
+    def get_stderr(self):
+        return self.stderr
+
+    def add_cgi_vars(self):
+        self.environ.update(self.base_env)
+
+    def _write(self,data):
+        iris.cls('Grongier.PEX.WSGI').write(data)
+
+    def _flush(self):
+        self.stdout.flush()
+        self._flush = self.stdout.flush
