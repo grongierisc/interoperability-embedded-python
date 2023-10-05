8752a64d990c8728e9e7433b3836a06119c7cc05
grongierisc <guillaume.rongier@intersystems.com>
2023-10-05 17:10:18 +0200

    update ci

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -5,3 +5,7 @@ root_dir = d(d(abspath(__file__)))
 sys.path.append(root_dir)
 # add registerFiles to the path
 sys.path.append(join(join(root_dir, 'tests'), 'registerFiles'))
+
+from grongier.pex import Utils
+
+Utils.setup()
