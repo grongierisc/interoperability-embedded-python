e3dc08da9b84600a3328c91d12b81a8fef70c7d6
grongierisc <guillaume.rongier@intersystems.com>
2024-07-25 10:01:05 +0200

    refactor multi-sync request handling for improved readability and maintainability

--- demo/python/multi_sync/bp.py
+++ demo/python/multi_sync/bp.py
@@ -8,7 +8,8 @@ class MyMultiBP(BusinessProcess):
         msg_one = MyMessage(message="Message1")
         msg_two = MyMessage(message="Message2")
 
-        tuple_responses = self.send_multi_request_sync([("Python.MyMultiBO", msg_one), ("Python.MyMultiBO", msg_two)])
+        tuple_responses = self.send_multi_request_sync([("Python.MyMultiBO", msg_one),
+                                                        ("Python.MyMultiBO", msg_two)])
 
         self.log_info("All requests have been processed")
         for target,request,response,status in tuple_responses:
