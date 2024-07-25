a3dc83693406a00c0d34b8d2f0a57ca190c28bcb
grongierisc <guillaume.rongier@intersystems.com>
2024-07-25 13:07:10 +0200

    refactor async_ng code to introduce random delay in MyAsyncNGBO

--- src/iop/_business_host.py
+++ src/iop/_business_host.py
@@ -580,43 +580,41 @@ class _send_request_async_ng(asyncio.Future):
         self._iris_handle = host.iris_handle
         asyncio.create_task(self.send())
 
-    async def send_async(self):
-        # Call the synchronous function
-        self._iris_handle.dispatchSendRequestAsyncNG(self.target, self.host._dispatch_serializer(self.request), self.timeout, self.description)
-        
-        # Periodically check if the request is done
-        while not self._iris_handle.dispatchIsRequestDone():
-            await asyncio.sleep(0.1)  # Adjust the sleep duration as needed
-        
-        # Set the result of the Future
-        self.set_result("Request sent and completed")
-
     async def send(self):
+        # init parameters
         message_header_id = iris.ref()
         queue_name = iris.ref()
         end_time = iris.ref()
         request = self.host._dispatch_serializer(self.request)
-        self._iris_handle.dispatchSendRequestAsyncNG(self.target, request, self.timeout, self.description, message_header_id, queue_name, end_time)
+
+        # send request
+        self._iris_handle.dispatchSendRequestAsyncNG(
+            self.target, request, self.timeout, self.description,
+            message_header_id, queue_name, end_time)
+        
+        # get byref values
         self._message_header_id = message_header_id.value
         self._queue_name = queue_name.value
         self._end_time = end_time.value
 
         while not self._done:
             await asyncio.sleep(0.1)
-            self.done()
+            self.is_done()
 
         self.set_result(self._response)
 
-    def done(self):
+    def is_done(self):
         response = iris.ref()
-        status = self._iris_handle.dispatchIsRequestDone(self.timeout, self._end_time, self._queue_name, self._message_header_id, response)
+        status = self._iris_handle.dispatchIsRequestDone(self.timeout, self._end_time,
+                                                         self._queue_name, self._message_header_id,
+                                                         response)
+        
         self._response = self.host._dispatch_deserializer(response.value)
-        if status == 1 and self._response is not None:
-            self._done = True
 
-    # def __await__(self):
-    #     if not self._done:
-    #         self.done()
-    #         yield self
-    #     else:
-    #         return self._response
+        if status == 2: # message found
+            self._done = True
+        elif status == 1: # message not found
+            pass
+        else:
+            self._done = True
+            self.set_exception(RuntimeError(iris.system.Status.GetOneStatusText(status)))
