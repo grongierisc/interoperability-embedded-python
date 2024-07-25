1099983f8fc2b6a6fa0a0d8267497a446fa66647
grongierisc <guillaume.rongier@intersystems.com>
2024-07-25 13:06:46 +0200

    refactor async_ng code to introduce random delay in MyAsyncNGBO

--- demo/python/async_ng/bo.py
+++ demo/python/async_ng/bo.py
@@ -2,11 +2,13 @@ from iop import BusinessOperation
 from msg import MyMessage
 
 import time
+import random
 
 class MyAsyncNGBO(BusinessOperation):
     def on_message(self, request):
         print(f"Received message: {request.message}")
-        time.sleep(1)
-        return MyMessage(message=f"Hello, {request.message}")
+        rand = random.randint(1, 10)
+        time.sleep(rand)
+        return MyMessage(message=f"Hello, {request.message} after {rand} seconds")
 
         
--- demo/python/async_ng/bp.py
+++ demo/python/async_ng/bp.py
@@ -1,6 +1,6 @@
 import asyncio
-import sys,os 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..', '..', 'src')))
+import random
+
 from iop import BusinessProcess
 from msg import MyMessage
 
@@ -8,26 +8,18 @@ from msg import MyMessage
 class MyAsyncNGBP(BusinessProcess):
 
     def on_message(self, request):
-        import time
-        start_time = time.time()
+
         results = asyncio.run(self.await_response(request))
-        end_time = time.time()
-        print(f"Time taken: {end_time - start_time} seconds")
-        self.log_info(f"Time taken: {end_time - start_time} seconds")
+
         for result in results:
-            print(f"Received response: {result.message}")
+            self.log_info(f"Received response: {result.message}")
 
     async def await_response(self, request):
-        msg_one = MyMessage(message="Message1")
-        msg_two = MyMessage(message="Message2")
-
-        # use asyncio.gather to send multiple requests asynchronously
-        # using the send_request_async_ng method
-        tasks = [self.send_request_async_ng("Python.MyAsyncNGBO", msg_one, timeout=5),
-                 self.send_request_async_ng("Python.MyAsyncNGBO", msg_two, timeout=-1)]
+        # create 1 to 10 messages
+        tasks = []
+        for i in range(random.randint(1, 10)):
+            tasks.append(self.send_request_async_ng("Python.MyAsyncNGBO",
+                                                    MyMessage(message=f"Message {i}")))
 
         return await asyncio.gather(*tasks)
 
-if __name__ == "__main__":
-    bp = MyAsyncNGBP()
-    bp.on_message(None)
