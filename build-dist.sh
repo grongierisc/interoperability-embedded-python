b110d0cd7252bdbdb6f7c2cef81ce44302199c1f
grongierisc <guillaume.rongier@intersystems.com>
2024-10-31 09:59:32 +0100

    embedded community driver

--- build-dist.sh
+++ build-dist.sh
@@ -2,6 +2,14 @@
 
 rm -rf ./build
 
+packages=("intersystems_iris" "irisnative")
+for package in ${packages[@]};
+do
+    rm -f ./src/$package
+    package_path=`python -c "import importlib.util; print(importlib.util.find_spec('${package}').submodule_search_locations[0])"`
+    ln -s $package_path ./src/$package
+done
+
 set -eo pipefail
 
 PYTHON_BIN=${PYTHON_BIN:-python3}
@@ -12,5 +20,10 @@ set -x
 
 cd "$PROJECT"
 $PYTHON_BIN setup.py sdist bdist_wheel
+
+for package in ${packages[@]};
+do
+    rm -f ./src/$package
+done
 
 set +x
--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
 from setuptools import setup
 
-setup(
-    install_requires = [
-        "intersystems_iris @ https://github.com/intersystems-community/intersystems-irispython/releases/download/3.8.0/intersystems_iris-3.8.0-py3-none-any.whl"
-    ]
-)
+setup()
