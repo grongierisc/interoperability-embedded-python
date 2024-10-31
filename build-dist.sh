e8248b819e98ef77089efc1d0108a3127630f2eb
grongierisc <guillaume.rongier@intersystems.com>
2024-10-31 09:52:48 +0100

    Update intersystems_iris dependency to version 3.8.0 and clean up build script

--- build-dist.sh
+++ build-dist.sh
@@ -2,14 +2,6 @@
 
 rm -rf ./build
 
-packages=("iris" "intersystems_iris" "irisnative")
-for package in ${packages[@]};
-do
-    rm -f ./src/$package
-    package_path=`python -c "import importlib.util; print(importlib.util.find_spec('${package}').submodule_search_locations[0])"`
-    ln -s $package_path ./src/$package
-done
-
 set -eo pipefail
 
 PYTHON_BIN=${PYTHON_BIN:-python3}
@@ -20,10 +12,5 @@ set -x
 
 cd "$PROJECT"
 $PYTHON_BIN setup.py sdist bdist_wheel
-
-for package in ${packages[@]};
-do
-    rm -f ./src/$package
-done
 
 set +x
--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import setup
 
-setup()
+setup(
+    install_requires = [
+        "intersystems_iris @ https://github.com/intersystems-community/intersystems-irispython/releases/download/3.8.0/intersystems_iris-3.8.0-py3-none-any.whl"
+    ]
+)
