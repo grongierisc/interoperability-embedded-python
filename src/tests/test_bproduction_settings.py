a5741fbd419fce4f18b8707bd099d61e760fef7f
grongierisc <guillaume.rongier@intersystems.com>
2025-03-11 14:04:46 +0100

    Update intersystems_iris dependency to version 3.8.1 and adjust test path for production settings

--- src/tests/test_bproduction_settings.py
+++ src/tests/test_bproduction_settings.py
@@ -3,12 +3,12 @@ from iop._utils import _Utils
 
 import os
 
-import iris
-
 class TestProductionSettings:
     @classmethod
     def setup_class(cls):
-        path = os.path.join(iris.__getattribute__('__ospath'),'src/tests/registerFilesIop/settings.py')
+        path = 'registerFilesIop/settings.py'
+        # get path of the current fille script
+        path = os.path.join(os.path.dirname(__file__), path)
         _Utils.migrate(path)
         _Director.stop_production()
         _Director.set_default_production('Python.TestSettingProduction')
