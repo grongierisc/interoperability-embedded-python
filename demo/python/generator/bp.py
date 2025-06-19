765194682441de0ae7b65edb77e00df549f6bf21
grongierisc <guillaume.rongier@intersystems.com>
2025-06-19 10:59:28 +0200

    Refactor MyGeneratorProcess to use MyGenerator for requests and update mkdocs navigation to include Component Interaction

--- demo/python/generator/bp.py
+++ demo/python/generator/bp.py
@@ -1,20 +1,16 @@
 from typing import Any
 from iop import BusinessProcess
 
-from msg import MyGenerator, MyGeneratorResponse 
+from msg import MyGenerator 
 
 class MyGeneratorProcess(BusinessProcess):
 
     def on_request(self, request: Any) -> Any:
-        rsp = self.send_request_sync(
-            target="User.MyGeneratorOperation",
-            request=MyGeneratorResponse(my_other_string="Hello, World!"),
-            timeout=10,
-            description="My generator request")
         gen = self.send_generator_request(
             target="User.MyGeneratorOperation",
             request=MyGenerator(my_string="Hello, World!"),
             timeout=10,
             description="My generator request")
+        
         for response in gen:
             self.log_info(f"Received response: {response}")
